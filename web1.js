e312ee6cb5fd394bdf464abcd06b3b83ee097cc2
poojakhil123456 <poojaonmail12@gmail.com>
2025-03-13 16:24:06 +0530

    added web1

---
+++ web1.js
@@ -0,0 +1,27 @@
+this is my first commit for session 1- javascript
+
+ in this session i learnt that what is javascript and  Why Does It Dominate the Web?
+ JavaScript is the backbone of interactivity on the web. Unlike HTML (which structures content) and CSS (which styles it), JavaScript controls behavior, it makes web pages dynamic,
+  responsive, and interactive.
+  It runs in the browser and allows developers to add animations, handle user inputs, update content dynamically, and communicate with servers.
+  JavaScript is single-threaded, meaning it executes one operation at a time in a single call stack.
+
+  why does dominate the web ?
+
+  1. JavaScript is the only programming language that web browsers natively understand.
+  2.This universal support makes it unavoidable for web developers.
+  3. JavaScript is used by 97% of all websites worldwide (including giants like Google, Facebook, and Amazon).
+  4. JavaScript allows real-time updates, user interactions, and animations.
+  5. JavaScript on both the frontend and backend.
+  6.  JavaScript on both the frontend and backend.
+  7.- JavaScript has powerful libraries and frameworks like: - React.js , Angular & Vue.js , Express.js .
+  
+  1. JavaScript is Growing with AI & Machine Learning - 
+  JavaScript is evolving with AI & Machine Learning through powerful libraries like TensorFlow.js, Brain.js, and Synaptic.js. 
+  The combination of JavaScript and AI is making web apps smarter, faster, and more interactive.
+
+And also i learn about JIT(Just In Time) Compilation?
+ JIT Compilation is a hybrid approach that combines interpretation and  compilation to improve JavaScript execution speed.JIT combines both approaches – it compiles JavaScript into machine code while the program is running, making it much faster than traditional interpretation.
+  
+JavaScript is Single-Threaded because JavaScript executes code sequentially, one operation at a time. 
+
